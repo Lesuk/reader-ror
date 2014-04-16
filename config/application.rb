af53cc542936e7f34305d87d05ef74093e3aff63
Lesuk <lesuk93@yahoo.com>
2014-04-16 20:35:38 +0000

    change host

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module ReaderRor
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+    config.action_mailer.default_url_options = { host: 'ua-reader.herokuapp.com' }
   end
 end
