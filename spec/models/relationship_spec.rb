8a74684d38d8a4c7a1f121c87854b2e2dfeb781f
Lesuk <lesuk93@yahoo.com>
2014-04-17 12:49:01 +0000

    rewrite all specs

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -2,37 +2,28 @@ require 'spec_helper'
 
 describe Relationship do
 
-    let(:follower) { FactoryGirl.create(:user) }
-    let(:followed) { FactoryGirl.create(:user) }
-    let(:relationship) { follower.relationships.build(followed_id: followed.id) }
+  let(:follower) { FactoryGirl.create(:user) }
+  let(:followed) { FactoryGirl.create(:user) }
+  let(:relationship) { follower.relationships.build(followed_id: followed.id) }
 
-    subject { relationship }
+  subject { relationship }
 
-    it { should be_valid }
+  it { should be_valid }
 
-    describe "accessible attributes" do
-      it "should not allow access to follower_id" do
-          expect do
-            Relationship.new(follower_id: follower.id)
-          end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
-      end    
-    end
-
-    describe "follower methods" do    
-      it { should respond_to(:follower) }
-      it { should respond_to(:followed) }
-      its(:follower) { should == follower }
-      its(:followed) { should == followed }
+  describe "follower methods" do    
+    it { should respond_to(:follower) }
+    it { should respond_to(:followed) }
+    its(:follower) { should eq follower }
+    its(:followed) { should eq followed }
   end
 
   describe "when followed id is not present" do
-      before { relationship.followed_id = nil }
-      it { should_not be_valid }
-    end
+    before { relationship.followed_id = nil }
+    it { should_not be_valid }
+  end
 
-    describe "when follower id is not present" do
-      before { relationship.follower_id = nil }
-      it { should_not be_valid }
-    end
-
+  describe "when follower id is not present" do
+    before { relationship.follower_id = nil }
+    it { should_not be_valid }
+  end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,233 +1,239 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User pages" do
 
-	subject { page }
+  subject { page }
 
-	describe "index" do
+  describe "index" do
+    let(:user) { FactoryGirl.create(:user) }
+    before(:each) do
+      sign_in user
+      visit users_path
+    end
 
-		let(:user) { FactoryGirl.create(:user) }
+    it { should have_title('All users') }
+    it { should have_content('All users') }
 
-	    before do
-	      sign_in user
-	      visit users_path
-	    end
+    describe "pagination" do
 
-	    it { should have_selector('title', text: 'All users') }
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
 
-	    describe "pagination" do
+      it { should have_selector('div.pagination') }
 
-	      before(:all) { 30.times { FactoryGirl.create(:user) } }
-	      after(:all)  { User.delete_all }
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
+      end
+    end
 
-	      it { should have_selector('div.pagination') }
+    describe "delete links" do
 
-	      it "should list each user" do
-	        User.paginate(page: 1).each do |user|
-	          page.should have_selector('li', text: user.name)
-	        end
-	      end
-	    end
+      it { should_not have_link('delete') }
 
-	    describe "delete links" do
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
 
-	      it { should_not have_link('delete') }
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+  end
 
-	      describe "as an admin user" do
-	        let(:admin) { FactoryGirl.create(:admin) }
-	        before do
-	          sign_in admin
-	          visit users_path
-	        end
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
 
-	        it { should have_link('delete', href: user_path(User.first)) }
-	        it "should be able to delete another user" do
-	          expect { click_link('delete') }.to change(User, :count).by(-1)
-	        end
-	        it { should_not have_link('delete', href: user_path(admin)) }
-	      end
-	    end
+    before { visit user_path(user) }
 
-  	end
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
 
-	describe "signup page" do
-		before{visit signup_path}
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
 
-		it { should have_selector('h1', 	text: 'Sign up') }
-		it { should have_selector('title', 	text: full_title('Sign up')) }
-	end
+    describe "follow/unfollow buttons" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before { sign_in user }
 
-	describe "profile page" do
-		let(:user) { FactoryGirl.create(:user) }
-		let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
-    	let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+      describe "following a user" do
+        before { visit user_path(other_user) }
 
-		before{visit user_path(user)}
+        it "should increment the followed user count" do
+          expect do
+            click_button "Follow"
+          end.to change(user.followed_users, :count).by(1)
+        end
 
-		it { should have_selector('h1', 	text: user.name) }
-		it { should have_selector('title', 	text: user.name) }
+        it "should increment the other user's followers count" do
+          expect do
+            click_button "Follow"
+          end.to change(other_user.followers, :count).by(1)
+        end
 
-		describe "microposts" do
-	      it { should have_content(m1.content) }
-	      it { should have_content(m2.content) }
-	      it { should have_content(user.microposts.count) }
-	    end
+        describe "toggling the button" do
+          before { click_button "Follow" }
+          it { should have_xpath("//input[@value='Unfollow']") }
+        end
+      end
 
-	    describe "follow/unfollow buttons" do
-      		let(:other_user) { FactoryGirl.create(:user) }
-      		before { sign_in user }
+      describe "unfollowing a user" do
+        before do
+          user.follow!(other_user)
+          visit user_path(other_user)
+        end
 
-      		describe "following a user" do
-        		before { visit user_path(other_user) }
+        it "should decrement the followed user count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(user.followed_users, :count).by(-1)
+        end
 
-        		it "should increment the followed user count" do
-          			expect do
-            			click_button "Follow"
-          			end.to change(user.followed_users, :count).by(1)
-        		end
+        it "should decrement the other user's followers count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(other_user.followers, :count).by(-1)
+        end
 
-        		it "should increment the other user's followers count" do
-          			expect do
-            			click_button "Follow"
-          			end.to change(other_user.followers, :count).by(1)
-        		end
+        describe "toggling the button" do
+          before { click_button "Unfollow" }
+          it { should have_xpath("//input[@value='Follow']") }
+        end
+      end
+    end
+  end
 
-        		describe "toggling the button" do
-          			before { click_button "Follow" }
-          			it { should have_selector('input', value: 'Unfollow') }
-        		end
-      		end
+  describe "signup page" do
+    before { visit signup_path }
 
-      		describe "unfollowing a user" do
-        		before do
-          			user.follow!(other_user)
-          			visit user_path(other_user)
-        		end
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
 
-        		it "should decrement the followed user count" do
-          			expect do
-            			click_button "Unfollow"
-          			end.to change(user.followed_users, :count).by(-1)
-        		end
+  describe "signup" do
 
-        		it "should decrement the other user's followers count" do
-          			expect do
-            			click_button "Unfollow"
-          			end.to change(other_user.followers, :count).by(-1)
-        		end
+    before { visit signup_path }
 
-        		describe "toggling the button" do
-          			before { click_button "Unfollow" }
-          			it { should have_selector('input', value: 'Follow') }
-        		end
-      		end
-    	end
-	end
+    let(:submit) { "Create my account" }
 
-	describe "signup" do
-		before { visit signup_path }
-		let(:submit){"Create my account"}
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
 
-		describe "with invalid information" do
-			it "should not create a user" do
-				expect{ click_button submit }.not_to change(User, :count)
-			end
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
 
-			describe "after submitions" do
-				before { click_button submit }
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
 
-				it { should have_selector('title', text: "Sign up") }
-				it { should have_content('error') }
-				it { should_not have_content('Password digest')  }
-			end
-		end
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
 
-		describe "with valid information" do
-			before do
-				fill_in "Name", 	with: "Lesuk"
-				fill_in "Email", 	with: "lesuk93@yahoo.com"
-				fill_in "Password", with: "amator123"
-				fill_in "Confirmation", with: "amator123"
-			end
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end
+  end
 
-			it "should create a user" do
-				expect { click_button submit }.to change(User, :count).by(1)
-			end
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
 
-			describe "after saving a user" do
-				before {click_button submit}
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
 
-				let(:user){ User.find_by_email("lesuk93@yahoo.com") }
-				it { should have_selector('title', text: user.name) }
-				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-				it { should have_link('Sign out') }
-			end	
-		end
-	end
+    describe "with invalid information" do
+      before { click_button "Save changes" }
 
-	describe "edit" do
-	    let(:user) { FactoryGirl.create(:user) }
-	    before do 
-	    	sign_in user
-	    	visit edit_user_path(user) 
-	    end
+      it { should have_content('error') }
+    end
 
-	    describe "page" do
-	      it { should have_selector('h1',    text: "Update your profile") }
-	      it { should have_selector('title', text: "Edit user") }
-	      it { should have_link('change', href: 'http://gravatar.com/emails') }
-	    end
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
 
-	    describe "with invalid information" do
-	      before { click_button "Save changes" }
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { expect(user.reload.name).to  eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
+    end
 
-	      it { should have_content('error') }
-	    end
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+      end
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
+      specify { expect(user.reload).not_to be_admin }
+    end
+  end
 
-	    describe "with valid information" do
-	      let(:new_name)  { "New Name" }
-	      let(:new_email) { "new@example.com" }
-	      before do
-	        fill_in "Name",             with: new_name
-	        fill_in "Email",            with: new_email
-	        fill_in "Password",         with: user.password
-	        fill_in "Confirm Password", with: user.password
-	        click_button "Save changes"
-	      end
+  describe "following/followers" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
 
-	      it { should have_selector('title', text: new_name) }
-	      it { should have_selector('div.alert.alert-success') }
-	      it { should have_link('Sign out', href: signout_path) }
-	      specify { user.reload.name.should  == new_name }
-	      specify { user.reload.email.should == new_email }
-	    end
-	end
+    describe "followed users" do
+      before do
+        sign_in user
+        visit following_user_path(user)
+      end
 
-	describe "following/followers" do
-    	let(:user) { FactoryGirl.create(:user) }
-    	let(:other_user) { FactoryGirl.create(:user) }
-    	before { user.follow!(other_user) }
+      it { should have_title(full_title('Following')) }
+      it { should have_selector('h3', text: 'Following') }
+      it { should have_link(other_user.name, href: user_path(other_user)) }
+    end
 
-    	describe "followed users" do
-      		before do
-        		sign_in user
-        		visit following_user_path(user)
-      		end
+    describe "followers" do
+      before do
+        sign_in other_user
+        visit followers_user_path(other_user)
+      end
 
-	    	it { should have_selector('title', text: full_title('Following')) }
-	    	it { should have_selector('h3', text: 'Following') }
-	    	it { should have_link(other_user.name, href: user_path(other_user)) }
-    	end
-
-    	describe "followers" do
-      		before do
-        		sign_in other_user
-        		visit followers_user_path(other_user)
-      		end
-
-      		it { should have_selector('title', text: full_title('Followers')) }
-      		it { should have_selector('h3', text: 'Followers') }
-      		it { should have_link(user.name, href: user_path(user)) }
-    	end
-  	end
+      it { should have_title(full_title('Followers')) }
+      it { should have_selector('h3', text: 'Followers') }
+      it { should have_link(user.name, href: user_path(user)) }
+    end
+  end
 end
