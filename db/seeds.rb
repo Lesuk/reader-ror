f680a835dcb5d08a1220d2e034e6297a523e360f
Oleksandr <lesuk93@yahoo.com>
2018-08-06 00:31:10 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 users_list = [
-  [ "Lesuk", "lesuk93@yahoo.com", "lesuk", "19931993", true ],
-  [ "Дмитрo Воронов", "rhmgerm66an@ukr.net", "voronov", "19931993", false ],
-  [ "Олег Угрин", "rmgerma@ukr.net", "oleg", "19931993", false ],
-  [ "Romgerman", "romgerman@ukr.net", "romgerman", "19931993", false ],
-  [ "Roman Subrichack", "romg7ermn@ukr.net", "Subrichack", "19931993", false ],
-  [ "Maryana Pukas", "romg67rman@ukr.net", "Maryana", "19931993", false ],
-  [ "Alex Mekhovov", "romgera@ukr.net", "Mekhovov", "19931993", false ],
-  [ "Andrii Furmanets", "rmgerma@ukr.net", "Furmanets", "19931993", false ],
-  [ "Олег Угрин", "rmgerma@ukr.net", "oleg", "19931993", false ],
+  [ "Alex", "alex@yahoo.com", "alex", "somepass", true ],
 ]
 users_list.each do |user|
   User.create(name: user[0], email: user[1], login: user[2], password: user[3], password_confirmation: user[3], admin: user[4])
